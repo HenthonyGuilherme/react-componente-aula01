10845053824e164b6c88578c30bd9f8ddad4e04c
HenthonyGuilherme <henthonyguilhermeferreira.2021@gmail.com>
2021-10-13 22:09:29 -0300

    nova atividade

---
+++ mensagem-react/src/App.js
@@ -0,0 +1,14 @@
+import { Mensagem } from "./Mensagem.js";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className= 'App2'>
+        <Mensagem/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ mensagem-react/src/Mensagem.js
@@ -0,0 +1,4 @@
+import React from 'react';
+export const Mensagem = () => <h1>"O fracasso não acontece quando se erra, mas quando se desiste.” (Mario Segio Cortella)</h1>
+    
+
